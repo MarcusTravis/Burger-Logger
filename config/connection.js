6843497f8c0e465d665a9926756ff7064fd63450
Marcus <mpt789@gmail.com>
2020-06-15 23:39:55 -0700

    added views folder, index.handlebars, and main.handlebars

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql");
 
-let connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db",
-});
+// let connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "",
+//   database: "burgers_db",
+// });
 
 connection.connect(function (err) {
   if (err) {
@@ -20,10 +20,11 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
   } else {
     connection = mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: 'hacktheplanet',
-      database: 'todoagain_db'
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "",
+        database: "burgers_db"
     });
 };
 

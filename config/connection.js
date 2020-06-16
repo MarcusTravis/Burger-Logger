bfa53674403e4359eaef9036ac7bb6198e9d48d8
Marcus <mpt789@gmail.com>
2020-06-16 15:37:40 -0700

    added code to make front end look nice

--- config/connection.js
+++ config/connection.js
@@ -18,14 +18,6 @@ connection.connect(function (err) {
 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
-  } else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: "",
-        database: "burgers_db"
-    });
-};
+  }
 
 module.exports = connection;

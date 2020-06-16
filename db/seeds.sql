127f050e3394b8e61e8a772375b65d493c336346
Marcus <mpt789@gmail.com>
2020-06-15 17:23:59 -0700

    added database, and table. Also added seeds.

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Cheese Burger');
--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const PORT = process.env.PORT || 3000;

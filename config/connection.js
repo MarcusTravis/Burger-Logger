5a51750641f6ee1e8a5d28852a48fe79476046d5
Marcus <mpt789@gmail.com>
2020-06-16 00:37:13 -0700

    added hello world to index.handlebars and changed config to get code running

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,19 @@
 const mysql = require("mysql");
 
-// let connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "",
-//   database: "burgers_db",
-// });
+let connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "burgers_db",
+});
 
 connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
   }
-  console.log("Connected to localhost, port & id: " + connection.port + connection.threadId);
+  console.log("Connected to localhost")
 });
 
 if (process.env.JAWSDB_URL) {
--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const app = express();
+const exphbs = require("express-handlebars");
+const routes = require("./controllers/burgers_controller.js");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
